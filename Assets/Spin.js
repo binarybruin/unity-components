e2f57fa03214744fd63881703ba6907811d59c27
binarybruin <michelle.bruin@gmail.com>
2015-04-09 16:41:07 -0400

    try to fix local rotation when moving, still not working (axis changes so moving gets complicated)

--- Assets/Spin.js
+++ Assets/Spin.js
@@ -10,13 +10,13 @@ function Update() {
 function Spin(speed, axis) {		// axis: x = 0, y = 1, z = 2
 	switch (axis) {
 		case 0:
-			transform.Rotate(speed * Time.deltaTime, 0, 0);
+			transform.Rotate(new Vector3(speed * Time.deltaTime, 0, 0), Space.Self);
 			break;
 		case 1:
-			transform.Rotate(0, speed * Time.deltaTime, 0);
+			transform.Rotate(new Vector3(0, speed * Time.deltaTime, 0), Space.Self);
 			break;
 		case 2:
-			transform.Rotate(0, 0, speed * Time.deltaTime);
+			transform.Rotate(new Vector3(0, 0, speed * Time.deltaTime), Space.Self);
 			break;
 		default:
 			break;
